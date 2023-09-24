a9290b7fe443044eab8789f73a4de36ad7c1c211
Dustin Smith <dusmith0@tamu.edu>
2023-09-24 16:47:59 -0500

    I added a new file to store the functions that I build in the original file. (Litterelty copied and pasted them, with some comments).

---
+++ Functions_for_Kmeans.R
@@ -0,0 +1,25 @@
+### This file contains functions that can be used in FunctionsKmeans.R
+
+##For calculating the clusters:
+diff_clusters <- function(Data,M){
+  diff <- apply(as.matrix(Data),c(1:2),function(Data) {sqrt((Data - M)^2)})
+  clusters <- apply(diff,2,function(z) which(z == min(z)))
+  #return(diff)
+  return(clusters)
+}
+
+##This is my first attempt at building a For loop for what the above did.
+Y <- c(rep(0,length(X)))
+
+for (i in length(X)){ 
+  diff <- rep(0,K)
+  diff <- (sqrt((X[i] - c(M))^2))
+  #return(diff)
+  assign("Y[i]",which(diff == min(diff)),envir = .GlobalEnv)
+  #return(Y)
+}
+
+Y
+
+##Note that while it does work, it will not retain the results in Y. The Y in the 
+##Global environment will be kept. 
