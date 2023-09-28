84fba234124ffb3341261eabd5d64c76a7a68d89
Dustin Smith <dusmith0@tamu.edu>
2023-09-28 17:11:53 -0500

    I moved my clusters into the function

--- FunctionsKmeans.R
+++ FunctionsKmeans.R
@@ -5,17 +5,13 @@
 # numIter - number of maximal iterations for the algorithm, the default value is 100
 
 
-
-# Building a function to place into an apply statement
-
-
-
 MyKmeans <- function(X, K, M = NULL, numIter = 100){
  
   # Check whether M is NULL or not. If NULL, initialize based on K randomly 
   # selected points from X.
 
   if(is.null(M)){
+    M <- rep(0,K)
     ifelse(!is.matrix(X), 
            (M <- sample(X,K,replace=FALSE)) , 
            ifelse(is.matrix(X),
@@ -25,6 +21,8 @@ MyKmeans <- function(X, K, M = NULL, numIter = 100){
            )
     )
   } 
+  return(M)
+}
   # This is about 300 nanoseconds shorter then what is below. Not sure if that is 
   # really worth the effort. Also if M is not a matrix or vector, I am not certain what this will do.
   
@@ -58,10 +56,10 @@ MyKmeans <- function(X, K, M = NULL, numIter = 100){
   Mnew <- c(rep(NULL,K))
   counter <- 0
 
-
   # Implement K-means algorithm.
   # Break option (ii) the maximal number of iterations was reached
   while(counter != numIter){
+    
     # Creating a Counter and merging the Mnew with M
     counter <- counter + 1
 
@@ -78,22 +76,26 @@ MyKmeans <- function(X, K, M = NULL, numIter = 100){
           diff[i,j] <- norm((X[i,] - M[j,]),type="2") 
         }
       }
+        # clusters <- apply(diff,2,function(z) which.min(diff)) #65 Microseconds
+        # This Piece is for re-evaluating the k-means
         clusters <- apply(diff,1,function(z) which(z == min(z)))
+        for(i in 1:K){
+          Mnew[i] <- mean(X[which(clusters == i),])
+        }
      }
         else {
           # For finding Euclidean Differences, and selecting the clusters
           diff <- apply(as.matrix(X),c(1,2),function(X) {sqrt((X - M) ^ 2)}) #104 microseconds
           #    diff <- apply(X,1,function (X) {norm((X - M),type="2")})
+          
+          # clusters <- apply(diff,2,function(z) which.min(diff)) #65 Microseconds
+          # This Piece is for re-evaluating the k-means
           clusters <- apply(diff,2,function(z) which(z == min(z))) #203 Microseconds
+          for(i in 1:K){
+            Mnew[i] <- mean(X[which(clusters == i)])
+          }
         }
       
-    }
-
-    # clusters <- apply(diff,2,function(z) which.min(diff)) #65 Microseconds
-    # This Piece is for re-evaluating the k-means
-
-    for(i in 1:K){
-      Mnew[i] <- mean(X[which(clusters == i),])
     }
 
     # Break option 1 the centroids don't change from one iteration to the next (exactly the same),
